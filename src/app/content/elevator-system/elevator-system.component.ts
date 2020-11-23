8a5d856f4c50494252b8a77dc291af4a741a8267
ManhHung020496 <hung.doan@orientsoftware.com>
2020-11-23 16:22:47 +0700

    new

--- src/app/content/elevator-system/elevator-system.component.ts
+++ src/app/content/elevator-system/elevator-system.component.ts
@@ -9,144 +9,160 @@ import { elevator } from './elevator.model';
 
 export class ElevatorSystemComponent implements OnInit {
 
-  list:any[]=[]
+  list: any[] = []
   constructor() { }
-  numberElevatorOne:any[]=[
+  numberElevatorOne: any[] = [
     {
-      floor:10,
-      status:false
+      floor: 10,
+      status: false
     },
     {
-      floor:9,
-      status:false
+      floor: 9,
+      status: false
     },
     {
-      floor:8,
-      status:false
+      floor: 8,
+      status: false
     },
     {
-      floor:7,
-      status:false
+      floor: 7,
+      status: false
     },
     {
-      floor:6,
-      status:false
+      floor: 6,
+      status: false
     },
     {
-      floor:5,
-      status:false
+      floor: 5,
+      status: false
     },
     {
-      floor:4,
-      status:false
+      floor: 4,
+      status: false
     },
     {
-      floor:3,
-      status:false
+      floor: 3,
+      status: false
     },
     {
-      floor:2,
-      status:false
+      floor: 2,
+      status: false
     },
     {
-      floor:1,
-      status:false
+      floor: 1,
+      status: false
     },
     {
-      floor:'G',
-      status:false
+      floor: 'G',
+      status: false
     },
-    ] ;
-  numberElevatorTwo:any[]=[
+  ];
+  numberElevatorTwo: any[] = [
     {
-      floor:10,
-      status:false
-    },
-    {
-      floor:9,
-      status:false
+      floor: 10,
+      status: false
     },
     {
-      floor:8,
-      status:false
+      floor: 9,
+      status: false
     },
     {
-      floor:7,
-      status:false
+      floor: 8,
+      status: false
     },
     {
-      floor:6,
-      status:false
+      floor: 7,
+      status: false
     },
     {
-      floor:5,
-      status:false
+      floor: 6,
+      status: false
     },
     {
-      floor:4,
-      status:false
+      floor: 5,
+      status: false
     },
     {
-      floor:3,
-      status:false
+      floor: 4,
+      status: false
     },
     {
-      floor:2,
-      status:false
+      floor: 3,
+      status: false
     },
     {
-      floor:1,
-      status:false
+      floor: 2,
+      status: false
     },
     {
-      floor:'G',
-      status:false
+      floor: 1,
+      status: false
     },
-  ] ;
+    {
+      floor: 'G',
+      status: false
+    },
+  ];
 
   ngOnInit(): void {
-   for(let i = 0 ; i < this.numberElevatorTwo.length ; i++)
-   {
+    const stopStage = 4;
+    this.list = this.numberElevatorTwo;
 
-    this.list.push(this.numberElevatorTwo[i])
+    for (let i = 0; i < stopStage; i++) {
 
-    // setTimeout(
-    //    () =>{
-    //    },1000*(i+1)
-    //  );
-   }
-   this.selectFloor()
+
+      setTimeout(
+        () => {
+          this.list[i].status = true;
+          if (i !== 0 || i) {
+            this.list[i - 1].status = false;
+          }
+        }, 1000 * (i + 1)
+      );
+    }
+
+
+
+    // this.list.forEach((stage, index) => {
+    //   setTimeout(
+    //     () => {
+    //       stage.status = true;
+    //       this.list.push(stage);
+    //     }, 1000 * (index + 1)
+    //   )
+    // });
+    //  this.selectFloor()
   }
 
-  selectFloor()
-  {
-  for(let i = 0 ; i< this.list.length ; i++)
-  {
+  // selectFloor()
+  // {
+  // for(let i = 0 ; i< this.list.length ; i++)
+  // {
 
-    this.list.find(
-       (x:elevator)=>
-       {
-
-
-    setTimeout(
-       () =>{
-       x.status=true
-        // if(x.floor !=3)
-        // {
-        //   x.status =false
-        // }
-       },1000*(i+1)
-     );
-
-       }
-
-    );
+  //   this.list.find(
+  //      (x:elevator)=>
+  //      {
 
 
-  }
+  //   setTimeout(
+  //      () =>{
+  //      x.status=true
+  //       // if(x.floor !=3)
+  //       // {
+  //       //   x.status =false
+  //       // }
+  //      },1000*(i+1)
+  //    );
+
+  //      }
+
+  //   );
 
 
-  }
+  // }
+
+
+  // }
 
 
 }
